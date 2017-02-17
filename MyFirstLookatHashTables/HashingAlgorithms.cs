f4333c5fe64f9c54bfaf0f15e540fcb51ff8389c
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-02-17 16:22:55 +0700

    Added 3 methods to hash a string. 1. Additive hashing 2. DJB2 hashing 3. Folding Hashing.

---
+++ MyFirstLookatHashTables/HashingAlgorithms.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstLookatHashTables
+{
+    public class HashingAlgorithms
+    {
+        public int AdditiveHash(string input)
+        {
+            int output = 0;
+
+            foreach (char character in input)
+            {
+                unchecked
+                {
+                    output += character;
+                }
+            }
+
+
+            return output;
+        }
+
+        public int DJB2Hash(string value)
+        {
+            int hash = 5381;
+            int i = 0;
+
+            for (i = 0; i < value.Length; i++)
+            {
+                unchecked
+                {
+                    hash = (((hash << 5) + hash) + ((byte)value[i]));
+                }
+            }
+
+            return hash;
+        }
+
+        public int FoldingHash(string input)
+        {
+            int hashValue = 0;
+            int startIndex = 0;
+            int currentfourBytes;
+
+            do
+            {
+                currentfourBytes = GetNextBytes(startIndex, input);
+                unchecked
+                {
+                    hashValue += currentfourBytes;
+                }
+
+                startIndex += 4;
+
+            } while (currentfourBytes != 0);
+
+            return hashValue;
+        }
+
+        private int GetNextBytes(int startIndex, string str)
+        {
+            int currentFourBytes = 0;
+
+            currentFourBytes += GetByte(str, startIndex);
+            currentFourBytes += GetByte(str, startIndex + 1) << 8;
+            currentFourBytes += GetByte(str, startIndex + 2) << 16;
+            currentFourBytes += GetByte(str, startIndex + 3) << 24;
+
+            return currentFourBytes;
+        }
+
+        private int GetByte(string str, int index)
+        {
+            if (index < str.Length)
+            {
+                return (int)str[index];
+            }
+
+            return 0;
+        }
+    }
+}
--- MyFirstLookatHashTables/Program.cs
+++ MyFirstLookatHashTables/Program.cs
@@ -8,8 +8,19 @@ namespace MyFirstLookatHashTables
 {
     class Program
     {
+        private static HashingAlgorithms hasingalgos = new HashingAlgorithms();
         static void Main(string[] args)
         {
+            string input;
+
+            do
+            {
+                input = Console.ReadLine().ToString();
+                Console.WriteLine("Hashed Value uisng Additive hash is: " + hasingalgos.AdditiveHash(input));
+                Console.WriteLine("Hashed Value uisng DJB2 hash is: " + hasingalgos.DJB2Hash(input));
+                Console.WriteLine("Hashed Value uisng Folding hash is: " + hasingalgos.FoldingHash(input));
+            } while (input != "quit");
+
         }
     }
 }
